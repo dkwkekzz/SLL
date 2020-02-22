fe7cad068d2a4e6239643bac968be367f18f68c6
yootaekyoung <dkwkekzz@naver.com>
2020-02-22 22:30:56 +0900

    objectpool 추가

---
+++ SLL/Manage/ObjectPool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace SLL.Manage
+{
+    public class ObjectPool<TValue> where TValue : class, new()
+    {
+        private readonly ConcurrentBag<TValue> _objects = new ConcurrentBag<TValue>();
+
+        public Func<TValue> Generator { get; set; }
+        public Action<TValue> Disposer { get; set; }
+        public int Capacity => _objects.Count;
+
+        public ObjectPool(int capacity = 0, Func<TValue> objectGenerator = null, Action<TValue> objectDisposer = null)
+        {
+            this.Generator = objectGenerator;
+            this.Disposer = objectDisposer;
+
+            for (int i = 0; i != capacity; i++)
+            {
+                var item = objectGenerator?.Invoke() ?? new TValue();
+                _objects.Add(item);
+            }
+        }
+        
+        public TValue GetObject()
+        {
+               TValue item;
+            if (_objects.TryTake(out item))
+                return item;
+            
+            return Generator();
+        }
+
+        public void PutObject(TValue item)
+        {
+            Disposer?.Invoke(item);
+            _objects.Add(item);
+        }
+    }
+}
---
+++ SLL/Manage/PoolingObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SLL.Manage
+{
+    public abstract class PoolingObject<T> where T : PoolingObject<T>, new()
+    {
+        public static ObjectPool<T> Factory { get; } = new ObjectPool<T>();
+        public static T Create() => Factory.GetObject();
+        public static void Destroy(T obj) => Factory.PutObject(obj);
+    }
+}
