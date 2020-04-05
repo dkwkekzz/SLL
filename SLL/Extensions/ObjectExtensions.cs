11366c70b4c65c23a5d120af3f1fe71fc8c2e60c
yootaekyoung <dkwkekzz@naver.com>
2020-04-05 09:54:04 +0900

    약간카피

--- SLL/Extensions/ObjectExtensions.cs
+++ SLL/Extensions/ObjectExtensions.cs
@@ -23,5 +23,28 @@ namespace SLL.Extensions
             }
         }
 
-    }
+		public static T ToObject<T>(this IDictionary<string, object> source)
+			where T : class, new()
+		{
+			T someObject = new T();
+			Type someObjectType = someObject.GetType();
+
+			foreach (KeyValuePair<string, object> item in source)
+			{
+				someObjectType.GetProperty(item.Key).SetValue(someObject, item.Value, null);
+			}
+
+			return someObject;
+		}
+
+		public static IDictionary<string, object> AsDictionary(this object source, BindingFlags bindingAttr = BindingFlags.Public | BindingFlags.Instance)
+		{
+			return source.GetType().GetProperties(bindingAttr).ToDictionary
+			(
+				propInfo => propInfo.Name,
+				propInfo => propInfo.GetValue(source, null)
+			);
+
+		}
+	}
 }
